e32eb495623172365fd17c09cbfedcffef20d4d5
Vikrant Puppala <vikrant.puppala@databricks.com>
2023-12-11 14:16:53 +0530

    Address code review comments

--- src/main/java/com/databricks/jdbc/driver/DatabricksJdbcConstants.java
+++ src/main/java/com/databricks/jdbc/driver/DatabricksJdbcConstants.java
@@ -8,6 +8,7 @@ public final class DatabricksJdbcConstants {
       Pattern.compile("jdbc:databricks:\\/\\/([^/]*)(?::\\d+)?\\/*(.*)");
   static final Pattern HTTP_PATH_PATTERN = Pattern.compile(".*\\/warehouses\\/(.*)");
   static final Pattern HTTP_PATH_SQL_PATTERN = Pattern.compile("sql\\/(.*)");
+  public static final String JDBC_SCHEMA = "jdbc:databricks://";
   static final String DEFAULT_LOG_LEVEL = "INFO";
   static final String LOG_LEVEL = "loglevel";
   static final String LOG_PATH = "logpath";
