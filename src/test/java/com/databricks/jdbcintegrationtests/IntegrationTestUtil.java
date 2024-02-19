6f8549c9dd6718dee84abbad62352f2b7f1a5a84
Madhav Sainanee <madhav.sainanee@databricks.com>
2024-02-19 13:24:14 +0530

    adds schema

--- src/test/java/com/databricks/jdbcintegrationtests/IntegrationTestUtil.java
+++ src/test/java/com/databricks/jdbcintegrationtests/IntegrationTestUtil.java
@@ -24,6 +24,10 @@ public class IntegrationTestUtil {
     return System.getenv("DATABRICKS_CATALOG");
   }
 
+  public static String getDatabricksSchema() {
+    return System.getenv("DATABRICKS_SCHEMA");
+  }
+
   public static String getDatabricksUser() {
     return System.getenv("DATABRICKS_USER");
   }
