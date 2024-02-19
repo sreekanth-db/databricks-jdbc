0185474baf2e8aad17287a9ada22dfb03e59ec9f
Madhav Sainanee <madhav.sainanee@databricks.com>
2024-02-19 11:05:59 +0530

    adds comment

--- src/test/java/com/databricks/jdbc/integration/IntegrationTestUtil.java
+++ src/test/java/com/databricks/jdbc/integration/IntegrationTestUtil.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/** Utility class to support integration tests **/
 public class IntegrationTestUtil {
 
     public static String getDatabricksHost() {
