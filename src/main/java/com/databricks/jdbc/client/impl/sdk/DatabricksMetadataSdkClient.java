adf36a9edb542373ab9cdc201a4d37762d9a656c
Vikrant Puppala <vikrant.puppala@databricks.com>
2024-02-29 16:43:40 +0530

    use wildcard util

--- src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksMetadataSdkClient.java
+++ src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksMetadataSdkClient.java
@@ -148,8 +148,7 @@ public class DatabricksMetadataSdkClient implements DatabricksMetadataClient {
     }
     // TODO: Limit to 15 pairs to run quickly, remove after demo/find workaround
     while (catalogSchemaPairs.size() > 15) catalogSchemaPairs.poll();
-    String tableWithContext =
-        tableNamePattern == null || tableNamePattern.equals("%") ? "*" : tableNamePattern;
+    String tableWithContext = WildcardUtil.jdbcPatternToHive(tableNamePattern);
 
     List<List<Object>> rows = new CopyOnWriteArrayList<>();
     ExecutorService executorService = Executors.newFixedThreadPool(150);
