aa8a2a45a288a5f8141ff65b1f8639dd7d05ad48
Gopal Lal <gopal.lal@databricks.com>
2024-03-05 15:34:35 +0530

    fix delete session path

--- src/main/java/com/databricks/jdbc/client/impl/sdk/PathConstants.java
+++ src/main/java/com/databricks/jdbc/client/impl/sdk/PathConstants.java
@@ -5,7 +5,7 @@ public class PathConstants {
   private static final String BASE_PATH = "/api/2.0/sql/";
   public static final String SESSION_PATH = BASE_PATH + "sessions/";
   public static final String STATEMENT_PATH = BASE_PATH + "statements/";
-  public static final String DELETE_SESSION_PATH_WITH_ID = STATEMENT_PATH + "%s";
+  public static final String DELETE_SESSION_PATH_WITH_ID = STATEMENT_PATH + SESSION_PATH + "%s";
   public static final String STATEMENT_PATH_WITH_ID = STATEMENT_PATH + "%s";
   public static final String RESULT_CHUNK_PATH = STATEMENT_PATH_WITH_ID + "/result/chunks/%s";
 }
