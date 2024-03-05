11636e3c902426b34761e4a28dbf4855fc91f0fe
Gopal Lal <gopal.lal@databricks.com>
2024-03-05 14:25:01 +0530

    path fix

--- src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksSdkClient.java
+++ src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksSdkClient.java
@@ -116,7 +116,7 @@ public class DatabricksSdkClient implements DatabricksClient {
         new DeleteSessionRequest()
             .setSessionId(sessionId)
             .setWarehouseId(((Warehouse) warehouse).getWarehouseId());
-    String path = String.format(SESSION_PATH_WITH_ID, request.getSessionId());
+    String path = String.format(DELETE_SESSION_PATH_WITH_ID, request.getSessionId());
     Map<String, String> headers = new HashMap<>();
     workspaceClient.apiClient().DELETE(path, request, Void.class, headers);
   }
--- src/main/java/com/databricks/jdbc/client/impl/sdk/PathConstants.java
+++ src/main/java/com/databricks/jdbc/client/impl/sdk/PathConstants.java
@@ -4,8 +4,8 @@ public class PathConstants {
 
   private static final String BASE_PATH = "/api/2.0/sql/";
   public static final String SESSION_PATH = BASE_PATH + "sessions/";
-  public static final String SESSION_PATH_WITH_ID = SESSION_PATH + "%s";
   public static final String STATEMENT_PATH = BASE_PATH + "statements/";
+  public static final String DELETE_SESSION_PATH_WITH_ID = STATEMENT_PATH + "%s";
   public static final String STATEMENT_PATH_WITH_ID = STATEMENT_PATH + "%s";
   public static final String RESULT_CHUNK_PATH = SESSION_PATH_WITH_ID + "/result/chunks/%s";
 }
