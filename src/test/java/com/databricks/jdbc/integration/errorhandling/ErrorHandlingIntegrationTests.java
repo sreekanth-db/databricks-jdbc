451e06968b2032366b7c67165c7a5966d6fa3b43
Madhav Sainanee <madhav.sainanee@databricks.com>
2024-03-11 11:15:11 +0530

    formatting

--- src/test/java/com/databricks/jdbc/integration/errorhandling/ErrorHandlingIntegrationTests.java
+++ src/test/java/com/databricks/jdbc/integration/errorhandling/ErrorHandlingIntegrationTests.java
@@ -5,9 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import com.databricks.jdbc.core.DatabricksParsingException;
 import com.databricks.jdbc.core.DatabricksSQLException;
-import java.sql.*;
-
 import com.databricks.jdbc.core.DatabricksSQLFeatureNotSupportedException;
+import java.sql.*;
 import org.junit.jupiter.api.Test;
 
 public class ErrorHandlingIntegrationTests {
