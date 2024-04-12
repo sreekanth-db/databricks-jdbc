1f2bccaa3d591140f4e9d3b5f889f38780de80f8
Madhav Sainanee <madhav.sainanee@databricks.com>
2024-04-12 13:34:40 +0530

    accept endpoints

--- src/main/java/com/databricks/jdbc/driver/DatabricksConnectionContext.java
+++ src/main/java/com/databricks/jdbc/driver/DatabricksConnectionContext.java
@@ -95,6 +95,7 @@ public class DatabricksConnectionContext implements IDatabricksConnectionContext
     }
     return HTTP_CLUSTER_PATH_PATTERN.matcher(url).matches()
         || HTTP_WAREHOUSE_PATH_PATTERN.matcher(url).matches()
+        || HTTP_ENDPOINT_PATH_PATTERN.matcher(url).matches()
         || TEST_PATH_PATTERN.matcher(url).matches();
   }
 
