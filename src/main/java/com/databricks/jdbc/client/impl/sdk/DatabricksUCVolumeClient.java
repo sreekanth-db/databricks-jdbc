32c7d09bfd934815b2c9eaad34dde7ef57c7b36c
Agnipratim Nag <agnipratim.nag@databricks.com>
2024-05-30 16:28:54 +0530

    Added UCVolumeClient Interface and Class Implementation with Tests + prefixExists feature (#186)
    * Saving my work so far
    
    * One basic test case working in DriverTester
    
    * Updated working of prefixExists testcase with LIST files instead of SHOW VOLUMES
    
    * Added basic test functions for many methods in DriverTester
    
    * Saving my work - tests + prefix exists unit and parameterised test
    
    * Added Interface and Class implementation of UCVolumeClient along with tests
    
    * did mvn spotless:apply
    
    * Addressed Madhav and Samikshya's comments. Added a case sensitive toggle for the prefixExists function.
    
    * forgot mvn spotless apply
    
    * minor changes
    
    * minor changes
    
    * Implemented Madhav's overloading suggestion + added logger statements
    
    * minor changes
    
    * addressed comments

---
+++ src/main/java/com/databricks/jdbc/client/impl/sdk/DatabricksUCVolumeClient.java
@@ -0,0 +1,61 @@
+package com.databricks.jdbc.client.impl.sdk;
+
+import com.databricks.jdbc.client.IDatabricksUCVolumeClient;
+import java.sql.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Implementation for DatabricksUCVolumeClient */
+public class DatabricksUCVolumeClient implements IDatabricksUCVolumeClient {
+
+  private final Connection connection;
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(DatabricksSdkClient.class);
+
+  public DatabricksUCVolumeClient(Connection connection) {
+    this.connection = connection;
+  }
+
+  private String createListQuery(String catalog, String schema, String volume) {
+    return String.format("LIST '/Volumes/%s/%s/%s/'", catalog, schema, volume);
+  }
+
+  public boolean prefixExists(String catalog, String schema, String volume, String prefix)
+      throws SQLException {
+    return prefixExists(catalog, schema, volume, prefix, true);
+  }
+
+  @Override
+  public boolean prefixExists(
+      String catalog, String schema, String volume, String prefix, boolean caseSensitive)
+      throws SQLException {
+
+    LOGGER.info(
+        "Entering prefixExists method with parameters: catalog={}, schema={}, volume={}, prefix={}, caseSensitive={}",
+        catalog,
+        schema,
+        volume,
+        prefix,
+        caseSensitive);
+
+    String listFilesSQLQuery = createListQuery(catalog, schema, volume);
+
+    try (Statement statement = connection.createStatement()) {
+      ResultSet resultSet = statement.executeQuery(listFilesSQLQuery);
+      LOGGER.info("SQL query executed successfully");
+
+      boolean exists = false;
+      while (resultSet.next()) {
+        String fileName = resultSet.getString("name");
+        if (fileName.regionMatches(!caseSensitive, 0, prefix, 0, prefix.length())) {
+          exists = true;
+          break;
+        }
+      }
+      return exists;
+    } catch (SQLException e) {
+      LOGGER.error("SQL query execution failed", e);
+      throw e;
+    }
+  }
+}
