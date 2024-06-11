40af30547415c13a34121443c0e3ddde2db2ab0d
Bhuvan Rangoju <bhuvan.rangoju@databricks.com>
2024-06-11 15:17:59 +0530

    Removed print statement

--- src/test/java/com/databricks/jdbc/client/http/DatabricksHttpClientTest.java
+++ src/test/java/com/databricks/jdbc/client/http/DatabricksHttpClientTest.java
@@ -186,7 +186,6 @@ public class DatabricksHttpClientTest {
     when(request.getURI()).thenReturn(URI.create("TestURI"));
     when(mockHttpClient.execute(request)).thenReturn(closeableHttpResponse);
     assertEquals(closeableHttpResponse, databricksHttpClient.execute(request));
-    System.out.println(request.getURI());
   }
 
   @Test
