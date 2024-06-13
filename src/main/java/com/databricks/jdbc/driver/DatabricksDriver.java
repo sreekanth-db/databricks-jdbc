9fec752992e0c2cbdefc4994b60728ab9630fbdd
Bhuvan Rangoju <bhuvan.rangoju@databricks.com>
2024-06-13 19:32:50 +0530

    Added Log rotation

--- src/main/java/com/databricks/jdbc/driver/DatabricksDriver.java
+++ src/main/java/com/databricks/jdbc/driver/DatabricksDriver.java
@@ -7,12 +7,15 @@ import com.databricks.jdbc.core.DatabricksSQLException;
 import com.databricks.jdbc.telemetry.DatabricksMetrics;
 import com.databricks.sdk.core.UserAgent;
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.Properties;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.apache.logging.log4j.core.appender.rolling.DefaultRolloverStrategy;
+import org.apache.logging.log4j.core.appender.rolling.TimeBasedTriggeringPolicy;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
@@ -101,20 +104,38 @@ public class DatabricksDriver implements Driver {
     UserAgent.withOtherInfo(CLIENT_USER_AGENT_PREFIX, connectionContext.getClientUserAgent());
   }
 
-  public static void configureLogging(String logFilePath, Level logLevel) {
+  public static void configureLogging(String logDirectory, Level logLevel) {
     LoggerContext context = (LoggerContext) LogManager.getContext(false);
     Configuration config = context.getConfiguration();
     PatternLayout layout =
         PatternLayout.newBuilder()
             .withPattern("%d{yyyy-MM-dd HH:mm:ss} %-5p %c{1}:%L - %m%n")
             .build();
-    FileAppender appender =
-        FileAppender.newBuilder()
+
+    // Define the triggering policy based on date
+    TimeBasedTriggeringPolicy timeTrigger =
+        TimeBasedTriggeringPolicy.newBuilder()
+            .withInterval(1) // interval is set to 1 to roll over every day
+            .withModulate(true)
+            .build();
+
+    // Define the rollover strategy no more than 10 files are stored at a single moment. The oldest
+    // file is deleted to created space for the new file.
+    DefaultRolloverStrategy strategy =
+        DefaultRolloverStrategy.createStrategy("10", "1", null, null, null, true, config);
+
+    // Define the appender
+    RollingFileAppender appender =
+        RollingFileAppender.newBuilder()
             .setConfiguration(config)
             .withLayout(layout)
-            .withFileName(logFilePath)
-            .withName(logFilePath)
+            .withFileName(logDirectory + "/logfile-" + LocalDate.now() + ".log")
+            .withFilePattern(logDirectory + "/logfile-%d{yyyy-MM-dd}.log")
+            .withPolicy(timeTrigger)
+            .withStrategy(strategy)
+            .withName("RollingFile")
             .build();
+
     LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME);
     if (appender != null) {
       // Appender can be null if the parameters are incorrect; no error should be thrown.
