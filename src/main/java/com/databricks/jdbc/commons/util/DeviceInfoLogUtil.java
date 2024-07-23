fb5cff688c94adc06c5ad6d3c9881cd0f0eb2100
Gopal Lal <gopal.lal@databricks.com>
2024-07-23 19:28:41 +0530

    Add extra logging

--- src/main/java/com/databricks/jdbc/commons/util/DeviceInfoLogUtil.java
+++ src/main/java/com/databricks/jdbc/commons/util/DeviceInfoLogUtil.java
@@ -19,18 +19,20 @@ public class DeviceInfoLogUtil {
     String localeName =
         System.getProperty("user.language") + "_" + System.getProperty("user.country");
     String charsetEncoding = Charset.defaultCharset().displayName();
-    LoggingUtil.log(LogLevel.DEBUG, String.format("JVM Name: {%s}", jvmName));
+
+    LoggingUtil.log(LogLevel.INFO, String.format("Driver Version: {%s}", DriverUtil.getVersion()));
+    LoggingUtil.log(LogLevel.INFO, String.format("JVM Name: {%s}", jvmName));
     LoggingUtil.log(
-        LogLevel.DEBUG, String.format("JVM Specification Version: {%s}", jvmSpecVersion));
+        LogLevel.INFO, String.format("JVM Specification Version: {%s}", jvmSpecVersion));
     LoggingUtil.log(
-        LogLevel.DEBUG, String.format("JVM Implementation Version: {%s}", jvmImplVersion));
-    LoggingUtil.log(LogLevel.DEBUG, String.format("JVM Vendor: {%s}", jvmVendor));
-    LoggingUtil.log(LogLevel.DEBUG, String.format("Operating System Name: {%s}", osName));
-    LoggingUtil.log(LogLevel.DEBUG, String.format("Operating System Version: {%s}", osVersion));
-    LoggingUtil.log(LogLevel.DEBUG, String.format("Operating System Architecture: {%s}", osArch));
-    LoggingUtil.log(LogLevel.DEBUG, String.format("Locale Name: {%s}", localeName));
+        LogLevel.INFO, String.format("JVM Implementation Version: {%s}", jvmImplVersion));
+    LoggingUtil.log(LogLevel.INFO, String.format("JVM Vendor: {%s}", jvmVendor));
+    LoggingUtil.log(LogLevel.INFO, String.format("Operating System Name: {%s}", osName));
+    LoggingUtil.log(LogLevel.INFO, String.format("Operating System Version: {%s}", osVersion));
+    LoggingUtil.log(LogLevel.INFO, String.format("Operating System Architecture: {%s}", osArch));
+    LoggingUtil.log(LogLevel.INFO, String.format("Locale Name: {%s}", localeName));
     LoggingUtil.log(
-        LogLevel.DEBUG, String.format("Default Charset Encoding: {%s}", charsetEncoding));
+        LogLevel.INFO, String.format("Default Charset Encoding: {%s}", charsetEncoding));
     if (context.enableTelemetry()) {
       try {
         DatabricksUsageMetrics.exportUsageMetrics(
