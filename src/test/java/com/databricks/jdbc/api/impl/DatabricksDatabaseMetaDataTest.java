db6ea78900a2a04aa1ab05aa960cc761079f82bb
Gopal Lal <135012033+gopalldb@users.noreply.github.com>
2024-08-27 12:50:19 +0530

    Release new version (#417)

--- src/test/java/com/databricks/jdbc/api/impl/DatabricksDatabaseMetaDataTest.java
+++ src/test/java/com/databricks/jdbc/api/impl/DatabricksDatabaseMetaDataTest.java
@@ -768,7 +768,7 @@ public class DatabricksDatabaseMetaDataTest {
   @Test
   public void testGetDriverVersion() throws SQLException {
     String result = metaData.getDriverVersion();
-    assertEquals("0.9.1-oss", result);
+    assertEquals("0.9.2-oss", result);
   }
 
   @Test
