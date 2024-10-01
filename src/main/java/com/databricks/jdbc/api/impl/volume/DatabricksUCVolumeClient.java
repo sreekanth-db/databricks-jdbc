78ee4b100953c9f7dd224e6e9b8f24a17f726008
Jothi Prakash <jothi.prakash@databricks.com>
2024-10-01 13:48:09 +0530

    [PECO-1952] Add Javadoc to DatabricksUCVolumeClient#listObjects (#533)
    * Added java docs for the listObjects function
    
    * Made java doc descriptive
    
    * Fixed formatting

--- src/main/java/com/databricks/jdbc/api/impl/volume/DatabricksUCVolumeClient.java
+++ src/main/java/com/databricks/jdbc/api/impl/volume/DatabricksUCVolumeClient.java
@@ -246,6 +246,22 @@ public class DatabricksUCVolumeClient implements IDatabricksUCVolumeClient {
     return volumeExists(catalog, schema, volumeName, true);
   }
 
+  /**
+   * This functions lists all the files that fall under the specified prefix within the target
+   * folder in the specified volume. The prefix is checked with the word after the last / in the
+   * input Ex - 1. foo/bar will list all the files within foo folder having bar as prefix | 2.
+   * foo/bar/f will list all the files within the bar folder with prefix f | 3. foo/bar/ will list
+   * all the files within the bar folder with all prefix
+   *
+   * @param catalog the catalog name of the cloud storage
+   * @param schema the schema name of the cloud storage
+   * @param volume the UC volume name of the cloud storage
+   * @param prefix the prefix of the filenames to list. This includes the relative path from the
+   *     volume as the root directory
+   * @param caseSensitive a boolean indicating whether the check should be case-sensitive or not
+   * @return List<String> a list of strings indicating the filenames that start with the specified
+   *     prefix
+   */
   @Override
   public List<String> listObjects(
       String catalog, String schema, String volume, String prefix, boolean caseSensitive)
