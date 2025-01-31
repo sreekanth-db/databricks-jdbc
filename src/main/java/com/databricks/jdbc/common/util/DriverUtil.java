d036603b3835011dbecd8cfb5b36c5b8d8347e87
Gopal Lal <135012033+gopalldb@users.noreply.github.com>
2025-02-01 02:15:13 +0530

    Remove DBSQL version check (#665)
    * Make DBSQL version check optional
    
    * cleanup dbsql info
    
    * Fix minor
    
    * Fix Logging test

--- src/main/java/com/databricks/jdbc/common/util/DriverUtil.java
+++ src/main/java/com/databricks/jdbc/common/util/DriverUtil.java
@@ -4,15 +4,12 @@ import static com.databricks.jdbc.common.DatabricksJdbcConstants.IS_FAKE_SERVICE
 
 import com.databricks.jdbc.api.IDatabricksConnectionContext;
 import com.databricks.jdbc.api.internal.IDatabricksConnectionInternal;
-import com.databricks.jdbc.common.DatabricksClientType;
 import com.databricks.jdbc.exception.DatabricksSQLException;
 import com.databricks.jdbc.exception.DatabricksValidationException;
 import com.databricks.jdbc.log.JdbcLogger;
 import com.databricks.jdbc.log.JdbcLoggerFactory;
 import com.databricks.jdbc.model.telemetry.enums.DatabricksDriverErrorCode;
-import com.google.common.annotations.VisibleForTesting;
 import java.io.IOException;
-import java.sql.ResultSet;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -59,7 +56,6 @@ public class DriverUtil {
       LOGGER.warn("Empty metadata client is being used.");
       connection.getSession().setEmptyMetadataClient();
     }
-    ensureUpdatedDBSQLVersionInUse(connection);
   }
 
   public static void setUpLogging(IDatabricksConnectionContext connectionContext)
@@ -100,81 +96,5 @@ public class DriverUtil {
       throwable = cause;
     }
     return throwable;
-  }
-
-  @VisibleForTesting
-  static void ensureUpdatedDBSQLVersionInUse(IDatabricksConnectionInternal connection)
-      throws DatabricksValidationException {
-    if (connection.getConnectionContext().getClientType() != DatabricksClientType.SEA
-        || isRunningAgainstFake()) {
-      // Check applicable only for SEA flow
-      return;
-    }
-    String dbsqlVersion = getDBSQLVersionCached(connection).trim();
-    if (WildcardUtil.isNullOrEmpty(dbsqlVersion)) {
-      // If the DBSQL version is not available, we cannot determine if the driver supports SEA.
-      // Proceeding with the connection.
-      return;
-    }
-    if (!doesDriverSupportSEA(dbsqlVersion)) {
-      String errorMessage =
-          String.format(
-              "Unsupported DBSQL version %s. Please update your compute to use the latest DBSQL version.",
-              dbsqlVersion);
-      LOGGER.error(errorMessage);
-      throw new DatabricksValidationException(errorMessage);
-    }
-  }
-
-  private static String getDBSQLVersionCached(IDatabricksConnectionInternal connection) {
-    String httpPath = connection.getConnectionContext().getHttpPath();
-    String version = cachedDBSQLVersions.get(httpPath);
-    if (version != null) {
-      LOGGER.debug("Using cached DBSQL Version for path %s: %s", httpPath, version);
-      return version;
-    }
-
-    synchronized (DriverUtil.class) {
-      version = cachedDBSQLVersions.get(httpPath);
-      if (version != null) {
-        return version;
-      }
-
-      version = queryDBSQLVersion(connection);
-      cachedDBSQLVersions.put(httpPath, version);
-      return version;
-    }
-  }
-
-  private static String queryDBSQLVersion(IDatabricksConnectionInternal connection) {
-    try (ResultSet resultSet = connection.createStatement().executeQuery(DBSQL_VERSION_SQL)) {
-      resultSet.next();
-      String dbsqlVersion = resultSet.getString(1);
-      LOGGER.debug("DBSQL Version in use: %s", dbsqlVersion);
-      return dbsqlVersion;
-    } catch (Exception e) {
-      LOGGER.info(
-          "Error retrieving DBSQL version: {%s}. Defaulting to minimum supported version.", e);
-      return getDefaultDBSQLVersion();
-    }
-  }
-
-  private static String getDefaultDBSQLVersion() {
-    return DBSQL_MIN_MAJOR_VERSION_FOR_SEA_SUPPORT + "." + DBSQL_MIN_MINOR_VERSION_FOR_SEA_SUPPORT;
-  }
-
-  private static boolean doesDriverSupportSEA(String dbsqlVersion) {
-    String[] parts = dbsqlVersion.split("\\.");
-    int majorVersion = Integer.parseInt(parts[0]);
-    int minorVersion = Integer.parseInt(parts[1]);
-    if (majorVersion == DBSQL_MIN_MAJOR_VERSION_FOR_SEA_SUPPORT) {
-      return minorVersion >= DBSQL_MIN_MINOR_VERSION_FOR_SEA_SUPPORT;
-    }
-    return majorVersion > DBSQL_MIN_MAJOR_VERSION_FOR_SEA_SUPPORT;
-  }
-
-  @VisibleForTesting
-  static void clearDBSQLVersionCache() {
-    cachedDBSQLVersions.clear();
   }
 }
