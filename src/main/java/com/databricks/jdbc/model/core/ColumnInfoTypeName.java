48089f2372406de147cdd799c3b7c0f89876e8bd
Sreekanth Vadigi <sreekanth.vadigi@databricks.com>
2025-09-02 20:00:51 +0530

    ResultSchema refactor
    Signed-off-by: Sreekanth Vadigi <sreekanth.vadigi@databricks.com>

---
+++ src/main/java/com/databricks/jdbc/model/core/ColumnInfoTypeName.java
@@ -0,0 +1,23 @@
+package com.databricks.jdbc.model.core;
+
+public enum ColumnInfoTypeName {
+  ARRAY,
+  BINARY,
+  BOOLEAN,
+  BYTE,
+  CHAR,
+  DATE,
+  DECIMAL,
+  DOUBLE,
+  FLOAT,
+  INT,
+  INTERVAL,
+  LONG,
+  MAP,
+  NULL,
+  SHORT,
+  STRING,
+  STRUCT,
+  TIMESTAMP,
+  USER_DEFINED_TYPE;
+}
---
+++ src/main/java/com/databricks/jdbc/model/core/ResultSchema.java
@@ -0,0 +1,55 @@
+package com.databricks.jdbc.model.core;
+
+import com.databricks.sdk.support.ToStringer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Collection;
+import java.util.Objects;
+
+public class ResultSchema {
+  @JsonProperty("column_count")
+  private Long columnCount;
+
+  @JsonProperty("columns")
+  private Collection<ColumnInfo> columns;
+
+  public ResultSchema setColumnCount(Long columnCount) {
+    this.columnCount = columnCount;
+    return this;
+  }
+
+  public Long getColumnCount() {
+    return this.columnCount;
+  }
+
+  public ResultSchema setColumns(Collection<ColumnInfo> columns) {
+    this.columns = columns;
+    return this;
+  }
+
+  public Collection<ColumnInfo> getColumns() {
+    return this.columns;
+  }
+
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    } else if (o != null && this.getClass() == o.getClass()) {
+      ResultSchema that = (ResultSchema) o;
+      return Objects.equals(this.columnCount, that.columnCount)
+          && Objects.equals(this.columns, that.columns);
+    } else {
+      return false;
+    }
+  }
+
+  public int hashCode() {
+    return Objects.hash(new Object[] {this.columnCount, this.columns});
+  }
+
+  public String toString() {
+    return (new ToStringer(ResultSchema.class))
+        .add("columnCount", this.columnCount)
+        .add("columns", this.columns)
+        .toString();
+  }
+}
